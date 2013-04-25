2a92ce9ad9128afdd6ba9e91ed77dbaee1813db7
Ross Kinney <r2624kel@comcast.net>
2013-04-25 11:07:50 -0600

    Changes to test_parallel_assignments_with_extra_values to get tests to pass
    [#48448957]

--- koans/about_array_assignment.rb
+++ koans/about_array_assignment.rb
@@ -14,8 +14,8 @@ class AboutArrayAssignment < Neo::Koan
 
   def test_parallel_assignments_with_extra_values
     first_name, last_name = ["John", "Smith", "III"]
-    assert_equal __, first_name
-    assert_equal __, last_name
+    assert_equal "John", first_name # first_name is set to the first item in array
+    assert_equal "Smith", last_name # last_name is set to the second item in array
   end
 
   def test_parallel_assignments_with_splat_operator
